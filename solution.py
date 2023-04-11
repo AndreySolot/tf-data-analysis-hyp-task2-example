950446e4b7714a704f8167f146a90db8d0354be8
Andrey Solot <114979763+AndreySolot@users.noreply.github.com>
2023-04-11 13:11:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 163596104 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    pval = sps.ks_2samp(x, y, alternative="two-sided").pvalue
+    effect = (pval < 0.09)
+    
+    return effect
+
