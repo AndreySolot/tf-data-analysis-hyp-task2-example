1f335c5246182bbda2919fb2d3caa5b0a5bd49e3
Andrey Solot <114979763+AndreySolot@users.noreply.github.com>
2023-04-11 19:28:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,8 @@ import scipy.stats as sps
 chat_id = 163596104 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    pval = sps.ks_2samp(x, y, alternative="two-sided").pvalue
-    effect = (pval < 0.09)
+    pvalue = sps.anderson_ksamp([x, y]).pvalue
+    effect = (pvalue < 0.09)
     
     return effect
 
